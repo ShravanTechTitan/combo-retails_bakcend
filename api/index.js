995edfeb6063ff5d476d9a294c4a3ec59b391215
Shravan kumar <shravankumar3849@gmail.com>
2025-08-29 07:41:45 +0530

    vercel.json add

---
+++ api/index.js
@@ -0,0 +1,42 @@
+// api/index.js
+import express from "express";
+import serverless from "serverless-http";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "../src/config/db.js";
+import userRoutes from "../src/routes/user.js";
+import productRoutes from "../src/routes/productRoutes.js";
+import partCategoryRoutes from "../src/routes/partCategoryRoutes.js";
+import deviceCategoryRoutes from "../src/routes/deviceCategoryRoutes.js";
+import brandRoutes from "../src/routes/brandRoutes.js";
+import modelRoutes from "../src/routes/modelRoutes.js";
+import searchRoutes from "../src/routes/search.js";
+
+dotenv.config();
+
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// DB connect
+connectDB();
+
+// Routes
+app.use("/api/users", userRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/partCategories", partCategoryRoutes);
+app.use("/api/deviceCategories", deviceCategoryRoutes);
+app.use("/api/brands", brandRoutes);
+app.use("/api/models", modelRoutes);
+app.use("/api", searchRoutes);
+
+// Optional root route
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+// Export for Vercel
+export default serverless(app);
+ 
