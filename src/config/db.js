982f9f27801ea2ae353043f0b8ace01bdde377af
Shravan kumar <shravankumar3849@gmail.com>
2025-08-29 21:44:12 +0530

    new update

--- src/config/db.js
+++ src/config/db.js
@@ -1,16 +1,8 @@
 import mongoose from "mongoose";
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+ const connectDB = async () => {
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB connected ✅");
-  } catch (err) {
-    console.error("DB connection error:", err.message);
-    process.exit(1); // stop app if DB fails
-  }
-};
+    };
 
 export default connectDB;
